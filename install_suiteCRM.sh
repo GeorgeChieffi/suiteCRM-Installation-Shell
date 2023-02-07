b35514f15294224493a097cbc2909e2ee6f37742
GeorgeChieffi <63757390+GeorgeChieffi@users.noreply.github.com>
2023-02-07 00:40:14 -0500

    Update install_suiteCRM.sh

--- install_suiteCRM.sh
+++ install_suiteCRM.sh
@@ -1,59 +1,48 @@
 #!/bin/bash
 
-# To run this command change permissions to executable (chmod 755 install_suiteCRM.sh)
-# call this command to run ./install_suiteCRM.sh
-
-
-# Update system packages
+# update the package list and upgrade the existing packages
 sudo apt update
 sudo apt upgrade -y
 
-# Install required dependencies
-sudo apt install -y apache2 mariadb-server libapache2-mod-php php-gd php-json php-curl php-mbstring php-intl php-mysql php-xml php-zip
+# install the required packages
+sudo apt install apache2 mariadb-server php php-mysql libapache2-mod-php unzip -y
 
-# Enable Apache mod_rewrite module
+# enable the Apache rewrite module
 sudo a2enmod rewrite
+
+# restart Apache
 sudo systemctl restart apache2
 
-# Secure MariaDB installation
-sudo mysql_secure_installation
-
-# Create database and user for SuiteCRM
-sudo mysql -u root <<EOF
+# create the database and user for SuiteCRM
+sudo mysql <<EOF
 CREATE DATABASE suitecrm;
-CREATE USER 'suitecrm_user'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'suitecrm'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON suitecrm.* TO 'suitecrm'@'localhost';
 FLUSH PRIVILEGES;
 EOF
 
-# Download and unzip SuiteCRM
-wget https://suitecrm.com/download/128/suite82/561949/suitecrm-8-2-3.zip
-unzip SuiteCRM-8-2-3.zip - /var/www/html/suitecrm
+# download and extract the latest version of SuiteCRM
+LATEST_VERSION=$(curl -s https://api.github.com/repos/salesagility/SuiteCRM/releases/latest | grep 'tag_name' | cut -d\" -f4)
+wget https://github.com/salesagility/SuiteCRM/releases/download/${LATEST_VERSION}/SuiteCRM-${LATEST_VERSION}.zip
+unzip SuiteCRM-${LATEST_VERSION}.zip
+sudo mv SuiteCRM-${LATEST_VERSION} /var/www/html/suitecrm
 
-# Set correct permissions for SuiteCRM folder
-sudo chown -R www-data:www-data /var/www/html/suitecrm
-sudo chmod -R 755 /var/www/html/suitecrm
-
-# Create virtual host configuration file
+# create a virtual host configuration for SuiteCRM
 sudo bash -c "cat > /etc/apache2/sites-available/suitecrm.conf <<EOF
 <VirtualHost *:80>
-    ServerAdmin admin@example.com
-    ServerName suiteserver.com
+    ServerName example.com
     DocumentRoot /var/www/html/suitecrm
+    ErrorLog \${APACHE_LOG_DIR}/error.log
+    CustomLog \${APACHE_LOG_DIR}/access.log combined
 
     <Directory /var/www/html/suitecrm>
-        Options Indexes FollowSymLinks
+        Options +FollowSymLinks
         AllowOverride All
         Require all granted
     </Directory>
-
-    ErrorLog \${APACHE_LOG_DIR}/error.log
-    CustomLog \${APACHE_LOG_DIR}/access.log combined
 </VirtualHost>
 EOF"
 
-# Enable virtual host
+# enable the virtual host and restart Apache
 sudo a2ensite suitecrm
-sudo systemctl reload apache2
-
-# Go to http://suiteserver.com/install.php to start the installation process 
+sudo systemctl restart apache2
